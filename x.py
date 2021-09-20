684bd3a1717d82af3676fa947d7743fe7605c843
psingh95 <ipawanjit@gmail.com>
2021-09-20 15:15:11 -0700

    first commit

---
+++ x.py
@@ -0,0 +1,11 @@
+import json
+import pandas as pd
+
+lake = pd.read_csv('addresses.csv')
+print(lake['Last'])
+
+""" with open('ex.json') as jsonFile:
+    jsonObject = json.load(jsonFile)
+    jsonFile.close()
+
+print(jsonObject["array"][0]['age']) """    
