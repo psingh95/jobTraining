d4fe8c8abcc6251fd3f749453d94bd1324ee64fc
psingh95 <ipawanjit@gmail.com>
2021-09-20 17:41:12 -0700

    first commit

--- x.py
+++ x.py
@@ -4,8 +4,6 @@ import pandas as pd
 lake = pd.read_csv('addresses.csv')
 print(lake['Last'])
 
-
-afsaasf
 """ with open('ex.json') as jsonFile:
     jsonObject = json.load(jsonFile)
     jsonFile.close()
