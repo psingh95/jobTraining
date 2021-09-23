010335dbef4740f8f529f4b1013af71ea9318766
psingh95 <ipawanjit@gmail.com>
2021-09-22 21:23:10 -0700

    first commit

--- testjenkins.py
+++ testjenkins.py
@@ -11,4 +11,8 @@ class Testjenkins(unittest.TestCase):
         self.assertEqual(jenkinsTest.hello_world(), 'Hello World')
 
 if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    ###########################################
     unittest.main()
