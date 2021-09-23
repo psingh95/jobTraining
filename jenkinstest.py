79ec3e19c6201f4d6690580a0da3710a2c6748e2
psingh95 <ipawanjit@gmail.com>
2021-09-23 08:34:19 -0700

    first commit

---
+++ jenkinstest.py
@@ -0,0 +1,5 @@
+def helloWorld():
+    return 'Hello World'
+
+def addTwo(num1, num2):
+    return num1 + num2
--- test_jenkins.py
+++ test_jenkins.py
@@ -1,14 +1,12 @@
 import unittest
 import jenkinstest
 
-jenkinsTest = jenkinstest.jenkins_test()
-
 class Testjenkins(unittest.TestCase):
     def test_addtwo(self):
-        self.assertEqual(jenkinsTest.add_two(10,5), 15)
+        self.assertEqual(jenkinstest.addTwo(10,5), 15)
 
     def test_split(self):
-        self.assertEqual(jenkinsTest.hello_world(), 'Hello World')
+        self.assertEqual(jenkinstest.helloWorld(), 'Hello World')
 
 if __name__ == '__main__':
     unittest.main()
