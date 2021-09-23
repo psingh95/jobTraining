4bb5f0dbf48842bc99fc4ad96f6c277c595a8b3b
psingh95 <ipawanjit@gmail.com>
2021-09-23 08:34:47 -0700

    first commit

--- test_jenkins.py
+++ test_jenkins.py
@@ -5,7 +5,7 @@ class Testjenkins(unittest.TestCase):
     def test_addtwo(self):
         self.assertEqual(jenkinstest.addTwo(10,5), 15)
 
-    def test_split(self):
+    def test_helloworld(self):
         self.assertEqual(jenkinstest.helloWorld(), 'Hello World')
 
 if __name__ == '__main__':
